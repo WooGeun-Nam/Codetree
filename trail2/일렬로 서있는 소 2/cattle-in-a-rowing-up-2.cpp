69d0e50aac58bc69709c74d72ff0e490d3677ea6
WooGeun-Nam <82949734+WooGeun-Nam@users.noreply.github.com>
2026-05-13 22:21:57 +0900

    [Passed] 일렬로 서있는 소 2 (8ms, 0MB)

---
+++ trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    vector<int> cow;
+
+    cin >> n;
+    
+    for(int i=0; i<n; i++)
+    {
+        int size;
+        cin >> size;
+        cow.push_back(size);
+    }
+
+    int answer = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(cow[i] <= cow[j])
+            {
+                for(int k=j+1; k<n; k++)
+                {
+                    if(cow[j] <= cow[k]) answer++;
+                }
+            }
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
